8b1c6fa9acf944fc2963c00a95d22e95fb45418a
lenakroeker <lenakroeker@gmail.com>
2020-12-03 19:37:15 -0500

    75% complete

--- server/handlers.js
+++ server/handlers.js
@@ -1,6 +1,6 @@
 "use strict";
 const { MongoClient } = require("mongodb");
-
+const assert = require("assert");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
@@ -8,6 +8,9 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
+//retrieve all seats data for redering seats
+
 const getSeats = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
   try {
@@ -31,4 +34,52 @@ const getSeats = async (req, res) => {
   }
 };
 
-module.exports = { getSeats };
+// book a seat
+
+const bookSeat = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("ticketworkshop");
+
+  const { seatId, fullName, creditCard, expiration, email } = req.body;
+  console.log(req.body);
+  var query = { _id: seatId };
+  const newValue = {
+    $set: {
+      isBooked: true,
+      fullName: fullName,
+      creditCard: creditCard,
+      expiration: expiration,
+      email: email,
+    },
+  };
+
+  console.log(newValue, "newvalue");
+  console.log(query, "query");
+
+  const seat = await db.collection("seats").findOne(query);
+  console.log(seat);
+  if (!seat.isBooked) {
+    try {
+      const result = await db.collection("seats").updateOne(query, newValue);
+      assert.equal(1, result.matchedCount);
+      assert.equal(1, result.modifiedCount);
+      res.status(200).json({ status: 200, _id: seatId });
+    } catch (error) {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ status: 500, data: req.body, message: error.message });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ status: 400, data: req.body, message: "seat already booked" });
+  }
+
+  client.close();
+};
+
+module.exports = { getSeats, bookSeat };
